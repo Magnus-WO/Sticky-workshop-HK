f0710f1785cee9f5745fb7a69d3bcf741caf16b8
Magnus Werner Ottesen <magnuswernerottesen@gmail.com>
2025-01-15 13:15:10 +0100

    implement feature for adding notes

---
+++ src/js/addNotes.js
@@ -0,0 +1,28 @@
+import { v4 as uuidv4 } from "uuid";
+
+//Function for creating a random number that controls the rotation of the notes
+export const generateRandomNumber = () => {
+  //Math.random() * (max - min +1) + min
+  return Math.floor(Math.random() * (10 - -10 + 1) + -10);
+};
+
+const addNotes = (subject, date, text) => {
+  const notesArray = JSON.parse(localStorage.getItem("notes")) || [];
+
+  const note = {
+    id: uuidv4(),
+    subject: subject.value.trim(),
+    date: date.value.trim(),
+    text: text.value.trim(),
+    rotation: generateRandomNumber(),
+  };
+  console.log(note);
+
+  notesArray.push(note);
+
+  console.log(notesArray);
+
+  //   storeNotes();
+};
+
+export default addNotes;
